38ed3360e83b93e326a0a99e1f25a98f2d74d59d
sebastiantofano <sebastianleoneltofano@hotmail.com>
2019-04-30 00:41:45 -0300

    Fork para el agregar Test

--- src/ar/edu/unlam/pb2/tp/Disco.java
+++ src/ar/edu/unlam/pb2/tp/Disco.java
@@ -3,46 +3,42 @@ package ar.edu.unlam.pb2.tp;
 public class Disco {
 	private Double radioInterno;
 	private Double radioExterno;
-	
 
-	public Disco(Double radioInterno, Double radioExterno) {
-		this.radioInterno = radioInterno;
-		this.radioExterno = radioExterno;
+	public Disco() {
 	}
 
+	public Double calcularPerimetroInterno(){
+		Double perimetroInterno;
+		perimetroInterno = 2 * radioInterno * Math.PI;
+		return perimetroInterno;
+	}
+	
+	public Double calcularPerimetroExterno() {
+		Double perimetroExterno;
+		perimetroExterno = 2 * radioExterno * Math.PI;
+		return perimetroExterno;
+	}
+	
+	public Double calcularSuperficie() {
+		Double superficie;
+		superficie = Math.PI * Math.pow(radioExterno, 2) - Math.PI * Math.pow(radioInterno, 2);
+		return superficie;
+	}
 
-	public Double getRadiointerno() {
+	public Double getRadioInterno() {
 		return radioInterno;
 	}
-
 
 	public void setRadioInterno(Double radioInterno) {
 		this.radioInterno = radioInterno;
 	}
 
-
 	public Double getRadioExterno() {
 		return radioExterno;
 	}
 
-
-	public void setRadioexterno(Double radioExterno) {
+	public void setRadioExterno(Double radioExterno) {
 		this.radioExterno = radioExterno;
 	}
-	
-	public Double getPerimetroInterno(Double radiointerno) {
-		Double perimetrointerno = 2 * radiointerno * Math.PI;
-		return perimetrointerno;
-	}
-	public Double getPerimetroExterno(Double radioExterno) {
-		Double perimetroExterno = 2 * radioExterno * Math.PI;
-		return perimetroExterno;
-	}
-	
-	public Double getArea(Double radioInterno, Double radioExterno) {
-		Double area = Math.PI*(Math.pow(radioExterno, 2)-Math.pow(radioInterno, 2));
-		return area;
-	}
-	
-	
+		
 }
